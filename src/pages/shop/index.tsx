be448e70da6ac3277848e1e04e5b61795eef4b8d
Gabriel Navas <gabnavas.dev@gmail.com>
2021-09-22 19:04:21 -0300

    book list shop

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,8 +1,23 @@
 import Header from '../../components/Header'
+import Item from '../../components/pages/shop/BookItem'
+
+import {
+  Container,
+  BookList
+} from './styles'
 
 const ShopPage = () => {
   return (
-    <Header />
+    <Container>
+      <Header />
+      <BookList>
+        {
+          Array(23)
+            .fill(null)
+            .map((_, index) => <Item key={index} />)
+        }
+      </BookList>
+    </Container>
   )
 }
 
--- src/pages/shop/styles.ts
+++ src/pages/shop/styles.ts
@@ -1,3 +1,16 @@
-import styles from 'styled-components'
+import styled from 'styled-components'
 
-export const Container = styles.div``
+export const Container = styled.li`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  max-width: 100vw;
+`
+
+export const BookList = styled.ul`
+  display: flex;
+  width: 86%;
+  flex-wrap: wrap;
+`

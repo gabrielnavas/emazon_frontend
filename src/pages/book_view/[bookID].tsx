f3c461f6ef26b25b87d089fd9f70f7b9c0f03630
Gabriel Navas <gabnavas.dev@gmail.com>
2021-09-24 01:36:51 -0300

    BookViewPage

--- src/pages/book_view/[bookID].tsx
+++ src/pages/book_view/[bookID].tsx
@@ -1,9 +1,44 @@
+import Image from 'next/image'
+
 import Header from '../../components/Header'
 import { makeEndpointAPI } from '../../config/api'
+
+import {
+  IconStarFill,
+  IconStarHalf,
+  IconStarEmpty
+} from '../../icons'
+import * as date from '../../utils/date'
 import { Book } from '../shop'
 
 import {
-  Container
+  Container,
+  Section,
+  Left,
+  Middle,
+  Right,
+  ImgBook,
+  HeaderMiddle,
+  HeaderTop,
+  Title,
+  TypeCover,
+  PublishedAt,
+  Edition,
+  Language,
+  Separator,
+  Author,
+  Feedback,
+  Stars,
+  CommentAmount,
+  BarSeparate,
+  Descrition,
+  PriceContainer,
+  Price,
+  PriceNoDiscount,
+  SavingContainer,
+  SavingAmount,
+  SavingsPercentage,
+  ButtonAddToCart
 } from './styles'
 
 type Props = {
@@ -14,7 +49,50 @@ const BookViewPage = ({ book }: Props) => {
   return (
     <Container>
       <Header />
-      <div>FALTA MONTAR INTERFACE MOSTRAR O BOOK</div>
+      <Section>
+        <Left>
+          <ImgBook>
+            <Image src='/img_book1.jpg' width={183} height={268} />
+          </ImgBook>
+        </Left>
+        <Middle>
+          <HeaderMiddle>
+            <HeaderTop>
+              <Title>{book.title}</Title>
+              <TypeCover>{book.typeCover.typeName}</TypeCover>
+              <PublishedAt> – {date.dayMonthNameYear(book.publishedAt)}</PublishedAt>
+            </HeaderTop>
+            <Edition>
+              <Language>Edição {book.language.name}</Language>
+              <Separator> | </Separator>
+              <Author>por <span>{book.author.name}</span> <span>(Autor)</span></Author>
+            </Edition>
+            <Feedback>
+              <Stars>
+                {Array(3).fill(<IconStarFill />)}
+                <IconStarHalf />
+                <IconStarEmpty />
+              </Stars>
+              <CommentAmount>17.123 avaliações de clientes</CommentAmount>
+            </Feedback>
+          </HeaderMiddle>
+          <BarSeparate />
+          <Descrition>{book.description}</Descrition>
+        </Middle>
+        <Right>
+          <PriceContainer>
+            <span>Comprar por</span>
+            <Price>R${(book.price * (1 - book.discount)).toFixed(2)}</Price>
+          </PriceContainer>
+          <PriceNoDiscount>De: R${book.price}</PriceNoDiscount>
+          <SavingContainer>
+            <span>Você economiza: R$</span>
+            <SavingAmount>{(book.price - (book.price * (1 - book.discount))).toFixed(2)}</SavingAmount>
+            <SavingsPercentage>({(((book.price * (1 - book.discount)) / book.price) * 100).toFixed(0)}%)</SavingsPercentage>
+          </SavingContainer>
+          <ButtonAddToCart>Adicionar ao carrinho</ButtonAddToCart>
+        </Right>
+      </Section>
     </Container>
   )
 }
--- src/pages/book_view/styles.ts
+++ src/pages/book_view/styles.ts
@@ -7,3 +7,183 @@ export const Container = styled.div`
   min-height: 100vh;
   max-width: 100vw;
 `
+
+export const Section = styled.div`
+  display: flex;
+  margin-top: 100px;
+`
+
+export const Left = styled.div`
+  width: 260px;
+`
+
+export const Middle = styled.div`
+  width: 640px;
+`
+
+export const Right = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 250px;
+  border: 1px #D5D9D9 solid;
+  border-radius: 8px;
+  padding: 14px 18px;
+`
+
+export const ImgBook = styled.div`
+`
+
+export const HeaderMiddle = styled.div`
+`
+
+export const HeaderTop = styled.div`
+  display: flex;
+  align-items: baseline;
+`
+
+export const Title = styled.div`
+  font-size: 28px;
+  line-height: 36px;
+  font-weight: 400;
+  color: #0F1111;
+  font-family: inherit;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+export const TypeCover = styled.div`
+  margin-left: 12px;
+  font-size: 24px;
+  line-height: 36px;
+  font-weight: 500;
+  color: #565959;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+export const PublishedAt = styled.div`
+  margin-left: 12px;
+  font-size: 21px;
+  line-height: 36px;
+  font-weight: 200;
+  color: #565959;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+export const Edition = styled.div`
+  display: flex;
+  font-size: 14px;
+  line-height: 36px;
+  font-weight: 200;
+  color: #0F1111;
+  font-family: Arial, Helvetica, sans-serif;
+`
+
+export const Language = styled.div`
+`
+
+export const Separator = styled.i`
+  color: #ddd;
+  margin: 0 5px 0 5px;
+
+  `
+
+export const Author = styled.div`
+  & span:nth-child(1) {
+    color: #007185;
+  }
+  & span:nth-child(2) {
+    color: #565959;
+  }
+`
+
+export const Feedback = styled.div`
+  display: flex;
+  justify-content: left;
+  margin: 5px 0 10px 0;
+  `
+
+export const Stars = styled.div`
+  & svg {
+    color: ${props => props.theme.default.colors.gold};
+  }
+`
+
+export const CommentAmount = styled.div`
+  margin-left: 20px;
+  font-size: 14px;
+  color: ${props => props.theme.default.colors.textBlue};
+`
+
+export const BarSeparate = styled.div`
+  margin-top: 10px;
+  border: 0.3px solid #ddd5;
+  width: 90%;
+`
+
+export const Descrition = styled.div`
+  margin-top: 20px;
+  color: #333333;
+`
+
+export const PriceContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: right;
+
+  & span {
+    max-width: 30%;
+    color: #0F1111;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 20px;
+  }
+`
+
+export const Price = styled.div`
+  display: flex;
+  justify-content: right;
+  align-items: top;
+  color: #B12704;
+  width: 70%;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 20px;
+`
+
+export const PriceNoDiscount = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: right;
+  color: #565959;
+  text-decoration: line-through;
+  margin-top: 5px;
+  font-size: 14px;
+`
+
+export const SavingContainer = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: right;
+  color: #565959;
+  margin-top: 5px;
+  font-size: 14px;
+`
+
+export const SavingAmount = styled.div`
+`
+
+export const SavingsPercentage = styled.div`
+`
+
+export const ButtonAddToCart = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 215px;
+  height: 35px;
+  background: #FFD814;
+  border-color: #FCD200;
+  border-radius: 20px;
+  cursor: pointer;
+  font-size: 14px;
+  margin-top: 20px;
+`

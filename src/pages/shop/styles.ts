d9e8d72e292524cc5ec31c54dc97b17665562e50
Gabriel Navas <gabnavas.dev@gmail.com>
2021-10-04 22:57:40 -0300

    dont have books style

--- src/pages/shop/styles.ts
+++ src/pages/shop/styles.ts
@@ -13,3 +13,15 @@ export const BookList = styled.ul`
   width: 86%;
   flex-wrap: wrap;
 `
+
+export const MessageDontHaveBooks = styled.a`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  color: #444;
+  font-weight: 700;
+  font-size: 30px;
+  padding: 50px;
+`
